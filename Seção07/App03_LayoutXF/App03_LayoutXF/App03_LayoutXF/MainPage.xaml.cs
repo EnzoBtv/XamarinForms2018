06c0e3aad9dca5c3fe36f7604d5f6251e931c3ff
enzo aurichi batrov <enzo_batrov@hotmail.com>
2018-11-25 20:14:28 -0200

    Projeto/Estudo de posicionamento completo

---
+++ Seção07/App03_LayoutXF/App03_LayoutXF/App03_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App03_LayoutXF
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+		private void MudarParaStack(object sender, EventArgs args)
+		{
+			Navigation.PushAsync(new Layouts.Stack.StackPage());
+		}
+		private void MudarParaGrid(object sender, EventArgs args)
+		{
+			Navigation.PushAsync(new Layouts.Grid.GridPage());
+		}
+		private void MudarParaAbsolute(object sender, EventArgs args)
+		{
+			Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+		}
+		private void MudarParaRelative(object sender, EventArgs args)
+		{
+			Navigation.PushAsync(new Layouts.Relative.RelativePage());
+		}
+		private void MudarParaScroll(object sender, EventArgs args)
+		{
+			Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+		}
+	}
+}
