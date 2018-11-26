b7fd2233b6ed981c44b4ada3250f38df8d66d5ab
enzo aurichi batrov <enzo_batrov@hotmail.com>
2018-11-25 23:00:29 -0200

    App04_HistoriaXamarin Concluido

---
+++ Seção07/App04_HistoriaXamarin/App04_HistoriaXamarin/App04_HistoriaXamarin/Master/Menu.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App04_HistoriaXamarin.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+		private void GoToPerfil1(object sender, EventArgs args)
+		{
+			Detail = new NavigationPage(new Pages.Perfil1()) {BarBackgroundColor= Color.FromHex("#ffc926") };
+			IsPresented = false;
+		}
+		private void GoToEmpresa(object sender, EventArgs args)
+		{
+			Detail = new NavigationPage(new Pages.Empresa()) { BarBackgroundColor = Color.FromHex("#ffc926") };
+			IsPresented = false;
+		}
+	}
+}
---
+++ Seção07/App04_HistoriaXamarin/App04_HistoriaXamarin/App04_HistoriaXamarin/Pages/Empresa.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App04_HistoriaXamarin.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Empresa : ContentPage
+	{
+		public Empresa ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
